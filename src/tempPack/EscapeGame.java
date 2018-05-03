d31ebc29be69418abdd5f7d574119e19e1b024bc
Ryan Castelli <ryancastelli@gmail.com>
2018-05-03 14:24:21 -0400

    first commit

---
+++ src/tempPack/EscapeGame.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tempPack;
+
+/**
+ * TEMP
+ * @author Ryan Castelli
+ */
+public class EscapeGame {
+    private EscapeGame() {
+        init();
+    }
+    private void init() {
+        
+    }
+}
